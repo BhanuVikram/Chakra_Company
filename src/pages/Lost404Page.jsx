0920d5d945f547176825dda7d8bdecd30ef65062
bhanuvikram <5193270+bhanuvikram@users.noreply.github.com>
2024-05-02 11:10:46 -0700

    Feature: Folders and pages with boilerplate code

---
+++ src/pages/Lost404Page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Lost404Page = () => {
+  return <div>Lost404Page</div>;
+};
+
+export default Lost404Page;
---
+++ src/pages/Lost500Page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Lost500Page = () => {
+  return <div>Lost500Page</div>;
+};
+
+export default Lost500Page;
