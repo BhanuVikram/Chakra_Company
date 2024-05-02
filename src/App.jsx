cd823f3992b60b4476f64b2640271b6fb120db05
bhanuvikram <5193270+bhanuvikram@users.noreply.github.com>
2024-05-02 11:58:07 -0700

    Feature: Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+// * ROUTING MODULES
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// * PAGES IMPORT
+import HomePage from "./pages/HomePage";
+import WebDevPage from "./pages/WebDevPage";
+import AppDevPage from "./pages/AppDevPage";
+import MarketingPage from "./pages/MarketingPage";
+import ContactPage from "./pages/ContactPage";
+import Lost404Page from "./pages/Lost404Page";
+import Lost500Page from "./pages/Lost500Page";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/web" element={<WebDevPage />} />
+      <Route path="/apps" element={<AppDevPage />} />
+      <Route path="/marketing" element={<MarketingPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/404" element={<Lost404Page />} />
+      <Route path="/500" element={<Lost500Page />} />
+      <Route path="/*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,21 @@ import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 
+// * ROUTING MODULES
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// * LAYOUTS IMPORT
+import HeaderLayout from "./layouts/HeaderLayout";
+import FooterLayout from "./layouts/FooterLayout";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <BrowserRouter>
+        <HeaderLayout />
+        <App />
+        <FooterLayout />
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
