1b531ce3a1d0ba1e4e344e23104fcf7bf587c075
Bhanu Vikram <5193270+BhanuVikram@users.noreply.github.com>
2024-05-24 22:03:03 -0700

    Feature: Marketing Services section

--- src/pages/MarketingPage.jsx
+++ src/pages/MarketingPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CarouselSectionComponent from "../components/CarouselSectionComponent";
+import MarketingServicesSectionComponent from "../components/MarketingServicesSectionComponent";
 
 const MarketingPage = () => {
   return (
     <div>
       <CarouselSectionComponent />
+      <MarketingServicesSectionComponent />
     </div>
   );
 };
